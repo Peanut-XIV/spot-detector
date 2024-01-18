b971bc3f9e6e170ac9e43c02291dc7261a6d7de6
Louis Lanoy <64585726+Peanut-XIV@users.noreply.github.com>
2024-01-18 17:41:01 +0100

    Fixed a bug preventing Pydantic from proceeding

--- spot_detector/types.py
+++ spot_detector/types.py
@@ -1,7 +1,7 @@
 from typing import TypeVar, TypeAlias, Union
 
-ColorTable:   TypeAlias = list[list[int, int, int, int]]
-Palette:      TypeAlias = list[list[int, int, int]]
+ColorTable:   TypeAlias = list[list[int]]
+Palette:      TypeAlias = list[list[int]]
 DataRow:      TypeAlias = list[Union[int, float, str]]
 DataTable:    TypeAlias = list[DataRow]
 ImageElement: TypeAlias = tuple[int, int, str]
