c62bf86ee883ac8cc4f9ec48e1e650413096f708
Louis Lanoy <64585726+Peanut-XIV@users.noreply.github.com>
2024-08-23 11:14:08 +0200

    Added remove-hot-pixels

--- spot_detector/__main__.py
+++ spot_detector/__main__.py
@@ -1,6 +1,7 @@
 import click
 
 from spot_detector.commands import detector, palette_editor
+from spot_detector.subtract_base_image import remove_hot_pixels
 
 
 @click.group
@@ -10,3 +11,4 @@ def cli():
 
 cli.add_command(detector)
 cli.add_command(palette_editor)
+cli.add_command(remove_hot_pixels)
