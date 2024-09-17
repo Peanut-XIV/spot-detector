01d49e609f068e2f4163e2dab41bf804cf8d0698
Louis Lanoy <64585726+Peanut-XIV@users.noreply.github.com>
2024-09-17 10:04:10 +0200

    Added gui and default image preview

---
+++ spot_detector/gui/main_window.py
@@ -0,0 +1,68 @@
+import sys
+import spot_detector.resources
+from importlib.resources import files
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QMainWindow,
+    QLabel,
+    QPushButton,
+    QGroupBox,
+    QVBoxLayout,
+    QWidget,
+    QScrollArea,
+)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Spot Detector GUI")
+
+        self._create_settings_panel()
+        self._create_image_panel()
+
+        layout = QHBoxLayout()
+        layout.addWidget(self._settings_panel)
+        layout.addWidget(self._image_panel)
+
+        self._central_widget = QWidget()
+
+        self._central_widget.setLayout(layout)
+        self.setCentralWidget(self._central_widget)
+
+
+    def _create_settings_panel(self):
+        self._settings_panel = QGroupBox("Settings")
+        layout = QVBoxLayout()
+        for i in range(10):
+            inline_layout = QHBoxLayout()
+            inline_layout.addWidget(QLabel(f"setting {i + 1}:"))
+            inline_layout.addWidget(QPushButton("button A"))
+            inline_layout.addWidget(QPushButton("button B"))
+            layout.addLayout(inline_layout)
+        layout.addStretch()
+        self._settings_panel.setLayout(layout)
+
+    def _create_image_panel(self):
+        default_image = QImage(str(files(spot_detector.resources).joinpath("testscreen.png")))
+        pixmap = QPixmap.fromImage(default_image)
+        label = QLabel("image")
+        label.setPixmap(pixmap)
+        self._image_panel = QScrollArea()
+        self._image_panel.setWidgetResizable(True)
+        self._image_panel.setMinimumSize(500, 500)
+        self._image_panel.setWidget(label)
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.resize(800, 600)
+    window.show()
+    sys.exit(app.exec())
+
+
