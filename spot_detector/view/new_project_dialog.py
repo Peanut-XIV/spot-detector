951ccf73672598785fb37083698fa3470659d677
Louis Lanoy <64585726+Peanut-XIV@users.noreply.github.com>
2026-01-29 15:21:21 +0100

    Moved PathLineWidget to it's own file

--- spot_detector/view/new_project_dialog.py
+++ spot_detector/view/new_project_dialog.py
@@ -1,6 +1,4 @@
 import sys
-from pathlib import Path
-from typing import Literal
 from PySide6.QtWidgets import (
     QApplication,
     QGridLayout,
@@ -20,8 +18,8 @@ from PySide6.QtCore import (
 from pydantic import ValidationError
 
 from spot_detector.model.project import Project
-from spot_detector.view.dialogs import OpenDirFileDialog, ReadOnlyImageFileDialog
 from spot_detector.view.welcome_window_interface import WelcomeWindowInterface
+from spot_detector.view.path_line_widget import PathLineWidget
 
 
 class NewProjectDialog(QDialog):
@@ -121,49 +119,6 @@ class NewProjectDialog(QDialog):
             message.exec()
             # raise validation error message
             # don't close dialog
-
-
-class PathLineWidget(QLineEdit):
-    def __init__(
-        self,
-        path_type: Literal["read_only_img", "dir", "other"],
-        caption: str = "",
-        starting_path: str | None = None,
-        parent: QWidget | None = None,
-    ) -> None:
-        super().__init__(parent)
-        if starting_path is not None:
-            self.starting_path = starting_path
-        else:
-            self.starting_path = str(Path.home())
-
-        self.caption = caption
-
-        self.path_type = path_type
-        if self.path_type == "read_only_img":
-            self.dialog = ReadOnlyImageFileDialog(
-                self, self.caption, self.starting_path
-            )
-        elif self.path_type == "dir":
-            self.dialog = OpenDirFileDialog(self, self.caption, self.starting_path)
-
-        self.setMinimumWidth(350)
-        self.explore_button = QPushButton("Explore", parent)
-        self.explore_button.clicked.connect(self.explore)
-
-    def set_starting_path(self, path: str):
-        self.starting_path = path
-
-    def get_button(self) -> QPushButton:
-        return self.explore_button
-
-    @Slot()
-    def explore(self):
-        self.dialog.setDirectory(self.starting_path)
-        if self.dialog.exec():
-            files = self.dialog.selectedFiles()
-            if len(files) > 0:
-                self.setText(files[0])
 
 
 if __name__ == "__main__":
